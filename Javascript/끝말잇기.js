a3d7da73042bc9354dc5f6180f05ef74b21e3424
WooBinJang <59834529+WooBinJang@users.noreply.github.com>
2020-09-14 19:11:39 +0900

    Update 끝말잇기.js

--- Javascript/끝말잇기.js
+++ Javascript/끝말잇기.js
@@ -1,14 +1,29 @@
-var startString = '바나나'; 
-var count =0;
-while(true) {
-var String = prompt(startString);
-if(String.length===3&&String[0]===startString[startString.length-1]){  
-    startString = String;
-} else{
-    count = count+1 // count++; 도 가능
-    if(count ==3 ){
-    alert('3번이상 틀렸습니다. 벌칙을 받으세요!!')    
-break;    
-		}
-	}
-}
+var 바디 = document.body;   // body 선택
+//document는 window가 지원하는 객체   html를 자바스크립트 언어로 번역하는 통역사 역활  DOM(Document Object Model)객체 
+var 단어 = document.createElement('div'); //createElement 태그 새로 만들기
+단어.textContent = '바나나'; 
+document.body.append(단어); // append  태그 추가
+var 폼 = document.createElement('form');
+document.body.append(폼);
+var 입력창 = document.createElement('input');
+폼.append(입력창);
+var 버튼 = document.createElement('button');
+버튼.textContent = '입력!';
+폼.append(버튼);
+var 결과창 = document.createElement('div');
+document.body.append(결과창);
+
+폼.addEventListener('submit', function 콜백함수 (e){
+	e.preventDefault();
+	if(입력창.value[0]===단어.textContent[단어.textContent.length-1]){  
+		결과창.textContent = '딩동댕';
+		단어.textContent= 입력창.value;
+		입력창.value= '';	
+		입력창.focus();
+	} else {
+		결과창.textContent = '땡';
+		입력창.value= '';	
+		입력창.focus();
+	} 
+	
+});
